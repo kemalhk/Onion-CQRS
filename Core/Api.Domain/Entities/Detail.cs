2f4d30e0fd196193af539e6a4ff794566860b05e
kemal <stormanq37@gmail.com>
2024-01-31 17:32:56 +0300

    entity

---
+++ Core/Api.Domain/Entities/Detail.cs
@@ -0,0 +1,29 @@
+﻿using Api.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Entities
+{
+    public class Detail : EntityBase
+    {
+        public Detail()
+        {
+                
+        }
+
+        public Detail(string title,string description,string categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+
+        public string Title{ get; set; }
+        public string Description{ get; set; }
+        public string CategoryId{ get; set; }
+        public Category Category{ get; set; }
+    }
+}
